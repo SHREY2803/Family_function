4e577ab503828a325a12ba0b0df76d66f2e679f7
Shrey Kulkarni <shrey280302@gmail.com>
2024-10-20 10:02:13 +0530

    Footer Section Space increased.

--- src/components/Footer.jsx/Footer.jsx
+++ src/components/Footer.jsx/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <>
       <div className="flex text-center justify-center ">
-        <span className="flex mb-5 bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-5xl tracking-tight text-transparent">
+        <span className="flex my-16 bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-5xl tracking-tight text-transparent">
           धन्यवाद
         </span>
         <div className="">
